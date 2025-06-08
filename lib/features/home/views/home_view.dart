fced04006460ead74b9d3bf1e584697682d89639
zainab <zenab.sen03@gmail.com>
2025-06-08 14:26:38 +0300

    creat home view

---
+++ lib/features/home/views/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:home_star_app/features/home/views/widgets/home_view_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeViewBody();
+  }
+}
