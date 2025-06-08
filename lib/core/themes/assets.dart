57e1586dd2864300016f90dbf8114134ff929da0
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-06-08 14:39:52 +0300

    Create Assets Folder

---
+++ lib/core/themes/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+}
+
